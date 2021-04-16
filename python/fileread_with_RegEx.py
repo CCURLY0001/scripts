ed8fcf7296742df1039abda0fc060af4cd3bc709
CCURLY <11825086+CCURLY0001@users.noreply.github.com>
2021-04-16 19:52:14 -0400

    Create fileread_with_RegEx.py

---
+++ python/fileread_with_RegEx.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import re
+
+filepath = str(input('Input Filepath: '))
+
+pattern = re.compile("(?<=\>).+?(?=\<)")
+
+if not os.path.isfile(filepath):
+    print("File path {} does not exist. Exiting...".format(filepath))
+    input('Press ENTER to exit')
+    sys.exit()
+
+with open(r"{}".format(filepath)) as fp:
+    for line in fp:
+        for match in re.finditer(pattern, line):
+            print(match.group())
+
+
+input('Press ENTER to exit')
