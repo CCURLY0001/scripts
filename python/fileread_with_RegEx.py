5dc41d560c07fa73d2ef23bd68d3c116da7274ed
CCURLY <11825086+CCURLY0001@users.noreply.github.com>
2021-04-16 23:03:21 -0400

    Update fileread_with_RegEx.py
    Add comments, Expand RegEx functionality, write to file2, etc, etc.

--- python/fileread_with_RegEx.py
+++ python/fileread_with_RegEx.py
@@ -2,19 +2,35 @@ import sys
 import os
 import re
 
-filepath = input('Input Filepath: ')
+import csv
 
-pattern = re.compile("(?<=\>).+?(?=\<)")
+# Define and check Filepath1 as an existing file path.
 
-if not os.path.isfile(filepath):
-    print("File path {} does not exist. Exiting...".format(filepath))
+filepath1 = input('Input existing file to check: ')
+
+if not os.path.isfile(filepath1):
+    print("File path {} does not exist. Exiting...".format(filepath1))
     input('Press ENTER to exit')
     sys.exit()
 
-with open(r"{}".format(filepath)) as fp:
-    for line in fp:
+# Define and open/create Filepath2 as the export file path
+filepath2 = input('Input full path of data export file: ')
+fp2 = open(r"{}".format(filepath2),'w')
+
+# Define REGEX Patterns
+pattern = re.compile("(?<=\>).+?(?=\<)" + "|" + "</tr>")
+
+# Open Filepath1 and run the main loop. Reads each line, finds pattern match, replaces </tr> with new lines.
+with open(r"{}".format(filepath1),'r') as fp1:
+    for line in fp1:
         for match in re.finditer(pattern, line):
-            print(match.group())
+            data = match.group()
+            if data == str("</tr>"):
+                data = str("\n")
+            print(data + ' written to {}'.format(filepath2))
+            fp2.writelines(data + ',')
 
 
+# Force User interaction for window close, cleanly close file
 input('Press ENTER to exit')
+filepath2.close()
