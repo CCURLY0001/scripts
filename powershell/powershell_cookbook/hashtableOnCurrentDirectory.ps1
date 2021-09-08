766645da880b69f4eda5172044967f5dbcc84b6d
CCURLY <11825086+CCURLY0001@users.noreply.github.com>
2021-09-08 19:38:49 -0400

    Create hashtableOnCurrentDirectory.ps1

---
+++ powershell/powershell_cookbook/hashtableOnCurrentDirectory.ps1
@@ -0,0 +1,24 @@
+<#
+ Study taken from Windows Powershell Cookbook Third Edition
+ Written By: Holmes
+ Published: O'Reilly
+#>
+
+<#
+Group Data as a Hash Table for repeated calling to provided hash
+
+Group-Object can be called to selected property as a hash on a pivot hashtable.
+
+Utilize the -AsString Paramater to prevent the need for explicit Int64 casts when calling the hash if it's of Int32 type.
+#>
+
+$curDir = dir | Group-Object -AsHash -AsString Length
+$curDir
+
+<#
+Name Value
+---- -----
+123  {filename.txt}
+#>
+
+$curDir["573"]
