fe7e93985d7423c67588a3a59e5d6027b3c9b860
CCURLY <11825086+CCURLY0001@users.noreply.github.com>
2022-01-08 14:15:08 -0500

    Update
    Updated to work properly with /bin/sh and log entries in vmkernel.log

--- bash/esxi/vm_power_on.sh
+++ bash/esxi/vm_power_on.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # Determines power state of a VM based on a Unique Identifier and boots it if not on
-# Designed for ESXi 6.7 U2 and tested on ESXi 6.7 U3
 
-# Designed due to the need of a temporary solution to prevent manual labor and intervention
-# while root cause of a recurring crash is determined. Set to run in cron jobs on
+esxcli system syslog mark --message="Running cron job for script powerOnVM.sh"
 
 # CHANGE VMUNIQUEIDENTIFIER AS NEEDED
 # Run vim-cmd vmsvc/getallvms to get a list of all vms and choose an identifying pattern
@@ -12,9 +10,12 @@ vmstatus=$(vim-cmd vmsvc/power.getstate $vmid)
 
 # Check if the VM is powered on
 if echo "$vmstatus" | grep -q "off" ; then
-	echo "Passed, powering on"
+	esxcli system syslog mark --message="VM Detected as off. Attempting to boot VM."
 	vim-cmd vmsvc/power.on $vmid
 elif echo "$vmstatus" | grep -q "on"; then
-	echo "Failed, already powered on"
+	esxcli system syslog mark --message="VM Detected as on. Exiting job."
+	break
+else
+	esxcli system syslog mark --message="Error running script - Exiting"
 	break
 fi
