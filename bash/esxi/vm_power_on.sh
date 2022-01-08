c04e45b99b5cd3b622ac93bb134ddb81bc558828
CCURLY <11825086+CCURLY0001@users.noreply.github.com>
2022-01-08 13:10:49 -0500

    Create vm_power_on.sh

---
+++ bash/esxi/vm_power_on.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Determines power state of a VM based on a Unique Identifier and boots it if not on
+
+# Designed due to the need of a temporary solution to prevent manual labor and intervention
+# while root cause of a recurring crash is determined.
+
+# CHANGE VMUNIQUEIDENTIFIER AS NEEDED
+# Run vim-cmd vmsvc/getallvms to get a list of all vms and choose an identifying pattern
+vmid=$(vim-cmd vmsvc/getallvms | awk '/VMUNIQUEIDENTIFIER/ {print $1}')
+vmstatus=$(vim-cmd vmsvc/power.getstate $vmid)
+
+# Check if the VM is powered on
+if echo "$vmstatus" | grep -q "off" ; then
+	echo "Passed, powering on"
+	vim-cmd vmsvc/power.on $vmid
+elif echo "$vmstatus" | grep -q "on"; then
+	echo "Failed, already powered on"
+	break
+fi
