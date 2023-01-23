7b8e1776d8e340c74eb04f33e8ddc38efe4fbba2
CCURLY <11825086+CCURLY0001@users.noreply.github.com>
2023-01-23 10:50:21 -0500

    Script legibility

--- powershell/sysadmin/Get_Services_and_Owner.ps1
+++ powershell/sysadmin/Get_Services_and_Owner.ps1
@@ -1,1 +1,3 @@
-Get-CimInstance -ClassName Win32_Service | Select-Object Name, DisplayName, StartMode, State, StartName, Description | Export-CSV C:\Users\ccolon\Desktop\test.Csv
+Get-CimInstance -ClassName Win32_Service | 
+Select-Object Name, DisplayName, StartMode, State, StartName, Description | 
+Export-CSV C:\Users\ccolon\Desktop\test.Csv
